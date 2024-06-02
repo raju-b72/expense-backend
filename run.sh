2deded49f1e132367c06968d30170721f6c60573
rajendra <rajendrammarboyana@gmail.com>
2024-06-02 11:40:34 +0100

    adding dockerfile for backend

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ node -r newrelic /app/index.js
 
 
 
-
+#these are conditions for backend env file.
 #Environment=DB_HOST="{{ rds_endpoint }}"
 #Environment=NEW_RELIC_APP_NAME=backend
 #Environment=NEW_RELIC_LICENSE_KEY={{NEWRELIC_KEY}}
